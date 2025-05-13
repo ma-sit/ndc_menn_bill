f1fc2b24dac42d66e9bc2b7657a5641e03f4ad61
simon <simonbille15@gmail.com>
2025-05-13 12:02:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,68 @@
-from pyxel import *
-import assets.pyxres
+import pyxel
 
-init(256, 256, title="Nuit du Code")
+class main:
+    def __init__(self):
+        pyxel.init(256, 256, title="naval", fps=30, quit_key=pyxel.KEY_ESCAPE)
+        self.x=128
+        self.y=128
+        self.r=0
+        self.id=[]
+        pyxel.run(self.update, self.draw)
 
+    def update(self):
+            self.ship()
+            self.direction()
 
+    def draw(self):
+        pyxel.cls(0)
+        blt(self.x, self.y, theme.pyxres, u, v, w, h, 5, r, 1 )
+        
+    def ship(self):
+        self.id=[]
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            self.x = self.x + 2
+            self.r=90
+            self.id.append(1)
 
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.x = self.x - 2
+            self.r=270
+            self.id.append(2)
+
+        if pyxel.btnp(pyxel.KEY_UP):
+            self.y = self.y - 2
+            self.r=0
+            self.id.append(3)
+
+        if pyxel.btnp(pyxel.KEY_DOWN):
+            self.y = self.y + 2
+            self.r=180
+            self.id.append(4)
+
+    def direction (self):
+        if len(self.id)!=1:
+            if 1 in self.id:
+                if 3 in self.id:
+                    self.r=0
+                    if 2 in self.id:
+                        self.r=90
+                    elif 4 in self.id:
+                        self.r=270
+                elif 2 in self.id:
+                    self.r=45
+                    if 4 in self.id:
+                        self.r=0
+                elif 4 in self.id:
+                    self.r=315
+            elif 2 in self.id:
+                if 3 in self.id:
+                    self.r=135
+                    if 4 in self.id:
+                        self.r=180
+                elif 4 in self.id:
+                    self.r=90
+            elif 3 in self.id:
+                if 4 in self.id:
+                    self.r=225
+
+main()
